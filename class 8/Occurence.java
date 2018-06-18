0f2a482850299c595b3b4fcf810d80f9f23f5978
sajan arora <sajanarora101@gmail.com>
2018-06-18 21:00:34 +0530

    Some file changes

--- class 8/Occurence.java
+++ class 8/Occurence.java
@@ -11,18 +11,20 @@ class Occurence{
 		char[] array1 = Sa.toCharArray();
 		char[] array2 = Sa1.toCharArray();
 int flag=0;
-			for(int i=0;i<array1.length;i++){
+			for(int i=0;i<array1.length-1;i++){
 				 //flag=0;
-				for(int j=0;j<array2.length;j++){
+				for(int j=0;j<array2.length-1;j++){
 					if(array1[i] == array2[j]){
 						flag++;}
 						else
 							flag=0;
 				}
 			}
-			if(flag==array2.length){
+			if(flag==array2.length)
 				System.out.println("yes the string is present");
-			}
+				else
+					System.out.println("no the string is not present");
+			
 			
 	}
 	
--- class 8/String_Vowel.java
+++ class 8/String_Vowel.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 class String_Vowel{
 	public static void main(String[] args){
-		/*Scanner data1 = new Scanner(System.in);
-		System.out.println("Enter any string to reverse it: ");
-		StringBuffer input_String = data1.nextLine();
-		*/
-		StringBuffer input = new StringBuffer("value");
-				char[] array1 = input.toString().toCharArray();
-				
-			StringBuffer str=new StringBuffer("") ;
-
-		for(int i=0;i<array1.length-1;i++){
-			if((array1[i] == 'a' || array1[i] == 'e' || array1[i] == 'i' || array1[i] == 'o' || array1[i] == 'u')){
-				str=str+input.deleteCharAt(i);
-			}
-		}
+		Scanner data1 = new Scanner(System.in);
+		System.out.println("Enter any string to remove the vowel from it : ");		
+		String input = data1.nextLine();
+		//take i/p in the form of String
 		
-			System.out.println();
+			StringBuffer str=new StringBuffer() ;
+//create a new StringBuffer str
+			
+			//the mechanism to remove vowels
+		for(int i=0;i<input.length();i++){
+			if(input.charAt(i) != 'a' && input.charAt(i) != 'e' && input.charAt(i) != 'i' && input.charAt(i) != 'o' && input.charAt(i) != 'u'){
+				str.append(input.charAt(i));
+				
+			}
+		}//end of loop
+		
+			System.out.println(str);
 	
 	}
 }
