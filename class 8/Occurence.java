094c4cf8f09b49506b47169f2457b4ff763a519d
sajan arora <sajanarora101@gmail.com>
2018-06-18 21:12:12 +0530

    Last update now

--- class 8/Occurence.java
+++ class 8/Occurence.java
@@ -5,8 +5,21 @@ class Occurence{
 		Scanner da = new Scanner(System.in);
 		System.out.println("Enter 1st string ");
 		String Sa = da.nextLine();
+		//take two Strings from the usr
 		System.out.println("Enter 2nd string to check in first string ");
 		String Sa1 = da.nextLine();	
+		
+		//boolean bs var that uses .contains method to verify the contents
+		
+			boolean bs = Sa.contains(Sa1);
+			if(bs){
+				//condn that works if is true
+				System.out.println("Yes the String2 is present in String1");
+			}
+			else
+				System.out.println("No the String2 is present in String1");
+		/*
+		tried this one but didnt worked
 		
 		char[] array1 = Sa.toCharArray();
 		char[] array2 = Sa1.toCharArray();
@@ -25,7 +38,7 @@ int flag=0;
 				else
 					System.out.println("no the string is not present");
 			
-			
+			*/	
 	}
 	
 }
