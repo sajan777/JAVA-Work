5333d4f8d707336407f87da82f4571b9a1b3577e
sajan arora <sajanarora101@gmail.com>
2018-06-19 15:19:53 +0530

    class 9

---
+++ class 9/File_Read.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class File_Read{
+	public static void main(String[] args) throws IOException{
+		//used IOException because the file may not exist
+		
+ 		
+		FileInputStream fis = new FileInputStream("C:\\Users\\Halfdevil\\Desktop\\JAVA Acadview\\class 9\\abc.txt");
+		//use the file input stream and add a path to access the file.
+		int c;
+		while(( c = fis.read()) != -1){
+		//loop to check till the end of the file
+		System.out.println((char)c);
+		//convert the int into char using (char)
+	}
+	}
+}
+	
