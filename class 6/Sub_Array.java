0dbe54d48f10543eb06d98eb383dfa9ed390d298
sajan arora <sajanarora101@gmail.com>
2018-06-19 14:54:08 +0530

    changes

--- class 6/Sub_Array.java
+++ class 6/Sub_Array.java
@@ -1,7 +1,24 @@
 class Sub_Array{
 	
 	public static void main(String[] args){
-		//not done will be done by the instructor
+		int val = 1,max=1;
+		int arr[] = {2,1,3,4,5,4,6,7,8,9,10}; 
+		for(int i=1;i<arr.length;i++){
+			if(arr[i]>arr[i-1])
+				val++;
+			
+			else{
+				if(max<val)
+				max=val;
+			val=1;
+			}
+		}
 		
+		if(max<val)
+		{
+			max=val;
+		
+	}
+	System.out.println(max);
 	}
 }
