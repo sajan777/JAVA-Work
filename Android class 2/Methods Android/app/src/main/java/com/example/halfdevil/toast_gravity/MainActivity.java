73483da507c05913ebd892d8e742f9f103e9dc82
sajan arora <sajanarora101@gmail.com>
2018-07-01 17:13:17 +0530

    Some changes

--- Android class 2/Methods Android/app/src/main/java/com/example/halfdevil/toast_gravity/MainActivity.java
+++ Android class 2/Methods Android/app/src/main/java/com/example/halfdevil/toast_gravity/MainActivity.java
@@ -7,18 +7,94 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-       //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+    //overriding the various android lifecycle methods
 
-     Toast t = Toast.makeText(getApplicationContext(),"The Message appears",Toast.LENGTH_SHORT);
-     //now t.setGravity is a method in the toast that allows the message to appear at what part of the
-        //screen
-     t.setGravity(Gravity.CENTER,0,0);
-     //t.show shows the message to display
-     t.show();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Created",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
+
+        protected void onRestart() {
+            super.onRestart();
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Restarted",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
+
+        protected void onPause() {
+            super.onPause();
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Paused",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
+        protected void onStop() {
+            super.onStop();
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Stopped",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
+        protected void onStart() {
+            super.onStart();
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Started",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
+        protected void onResume() {
+            super.onResume();
+            setContentView(R.layout.activity_main);
+
+            //creating a new toast and setting the parameters and saving the refrence in the var t of toast type
+
+            Toast t = Toast.makeText(getApplicationContext(),"Activity Resumed",Toast.LENGTH_SHORT);
+            //now t.setGravity is a method in the toast that allows the message to appear at what part of the
+            //screen
+            t.setGravity(Gravity.CENTER,0,0);
+            //t.show shows the message to display
+            t.show();
+        }
+
     }
-}
