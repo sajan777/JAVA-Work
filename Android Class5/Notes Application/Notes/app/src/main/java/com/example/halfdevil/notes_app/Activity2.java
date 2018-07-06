dbc3ac37b74d4a1fdbc779d39837c5e86b028478
sajan arora <sajanarora101@gmail.com>
2018-07-06 15:02:37 +0530

    Final

--- Android Class5/Notes Application/Notes/app/src/main/java/com/example/halfdevil/notes_app/Activity2.java
+++ Android Class5/Notes Application/Notes/app/src/main/java/com/example/halfdevil/notes_app/Activity2.java
@@ -1,11 +1,14 @@
 package com.example.halfdevil.notes_app;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -17,15 +20,15 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        editText = (EditText)findViewById(R.id.editText);
-        button = (Button)findViewById(R.id.button);
-        button3 = (Button)findViewById(R.id.button3);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button3 = (Button) findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intentObj = new Intent(Activity2.this,Activity3.class);
+                Intent intentObj = new Intent(Activity2.this, Activity3.class);
                 startActivity(intentObj);
             }
         });
@@ -34,11 +37,23 @@ public class Activity2 extends AppCompatActivity {
             public void onClick(View view) {
 
                 String save = editText.getText().toString();
-
-
+                newNote(save);
             }
         });
+    }
+
+        void newNote(String save){
+
+            try {
+                SQLiteDatabase database = this.openOrCreateDatabase("NewDatabase", MODE_PRIVATE, null);
+                database.execSQL("create table if not exists list (notetext VARCHAR)");
+
+                database.execSQL("insert into list values('" + save + "')");
+                Log.i("save","Done");
+                Toast.makeText(getApplicationContext(),"Added",Toast.LENGTH_SHORT).show();
+            }catch (Exception e){}
+        }
 
     }
 
-}
+
--- Android Class5/Notes Application/Notes/app/src/main/java/com/example/halfdevil/notes_app/Activity3.java
+++ Android Class5/Notes Application/Notes/app/src/main/java/com/example/halfdevil/notes_app/Activity3.java
@@ -1,19 +1,53 @@
 package com.example.halfdevil.notes_app;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Activity3 extends AppCompatActivity {
 
     EditText editText2;
+    ArrayList<String> save;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
 
-        editText2 = (EditText) findViewById(R.id.editText2);
+       // editText2 = (EditText) findViewById(R.id.editText2);
+        listView = (ListView)findViewById(R.id.list);
+
+        takeNotes();
+    }
+    void takeNotes(){
+        save = new ArrayList<String>();
+        try{
+            SQLiteDatabase database = this.openOrCreateDatabase("NewDatabase",MODE_PRIVATE,null);
+            Cursor cursor = database.rawQuery("select * from list",null);
+            int note = cursor.getColumnIndex("notetext");
+            cursor.moveToFirst();
+            while (cursor != null){
+                Log.i("save",cursor.getString(note));
+                save.add(cursor.getString(note));
+                cursor.moveToNext();
+            }
+
+        }catch (Exception e){}
+        finally {
+            {
+                arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,save);
+                listView.setAdapter(arrayAdapter);
+            }
+        }
     }
 }
